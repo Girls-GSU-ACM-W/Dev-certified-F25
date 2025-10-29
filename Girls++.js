898504c30f76bb585929aecacba8ae3219b9f0aa
CelineCherry <celinecherryan@gmail.com>
2025-10-29 17:25:36 -0400

    updated html and js

--- Girls++.js
+++ Girls++.js
@@ -1,14 +1,4 @@
 var taskList = [];
-/*
-Celine:
-Add tasks to a list once the "add task" button is pressed
-Save tasks to localStorage (happens by a callback once Fatemeh function 1 is finished)
-Load tasks from localStorage (happens on page being opened)
-Fatemeh:
-Sort the tasks in the tasklist when "add task" is pressed (via a function called inside Celine function 1)
-show tasks on screen by making copies of a task element (happens after Fatemeh function 1)
-*/
-
 
 class Task {
     constructor(name, due_date, duration, priority, desc, color){
@@ -20,44 +10,95 @@ class Task {
 
 
 function onAddTask(){
-    alert("exscript");
-    //take data and add an object to taskList
-    var name = document.getElementById("task_title").value;
-    var due_date = document.getElementById("due_date").value;
-    var duration = document.getElementById("time").value;
-    var priority = document.getElementById("priority_select").value;
-    var desc = document.getElementById("task_desc").value;
-    var color = document.getElementById("color").value;
-    alert("exscript");
-    alert([name, due_date, duration, priority, desc, color]);
-    sortTaskList();
+    //take data and add an object to taskList    
+    sortTaskList()
 }
 
-function sortTaskList(){
-    //sort tasks in list
-    saveTaskList()
-    showTasks()
+function sortTaskList() {
+
+    for (var i = 0; i < taskList.length; i++) {
+        var task = taskList[i];
+
+
+        var dueDate = new Date(task.due_date);
+        var today = new Date();
+        var daysLate = Math.floor((today - dueDate) / (1000 * 60 * 60 * 24));
+        if (daysLate < 0) daysLate = 0;
+
+
+        var priorityValue= 1;
+        if (task.priority === "medium_priorty") {
+            priorityValue= 2;
+        } else if (task.priority === "high_priorty") {
+            priorityValue= 3;
+        }
+
+
+        var minutes= parseInt(task.duration) || 0;
+        task.score= (daysLate * 5) * (priorityValue * 10) * (minutes * 1);
+    }
+
+
+    taskList.sort(function(a, b) {
+        return b.score - a.score;
+    });
+
+
+    saveTaskList();
+    showTasks();
 }
-
-function loadTaskList(){
-
-
-    
-}
-
 
 
 function saveTaskList(){ // fatemeh
+    localStorage.setItem("tasks",JSON.stringify(taskList))
 
 }
 
+function loadTaskList(){
+    var savedtasks= localStorage.getItem("tasks");
+    if (savedtasks) {
+        taskList= JSON.parse(savedtasks);
+    } else {
+        taskList= [];
+    }
 
-
-function showTasks(){ //fatemeh
-    for(task in taskList){
-        
-    } 
 }
 
-loadTaskList();
-sortTaskList();
+function showTasks() { // fatemeh
+    var displayArea = document.querySelector(".task_display");
+    displayArea.innerHTML = "";
+
+    for (var i = 0; i < taskList.length; i++) {
+        var task = taskList[i];
+
+        var taskDiv = document.createElement("div");
+        taskDiv.className = "color-target";
+        taskDiv.style.backgroundColor = task.color;
+
+        taskDiv.innerHTML = `
+            <table style="width:100%;">
+                <tr>
+                    <th colspan="3">${task.name}</th>
+                    <th></th>
+                    <th style="color:rgb(63,62,62);">Completed</th>
+                </tr>
+                <tr>
+                    <td colspan="3">${task.desc}</td>
+                    <td></td>
+                    <td><input type="checkbox" ${task.completed ? "checked" : ""}></td>
+                </tr>
+                <tr>
+                    <td colspan="3"><p>${task.duration} mins</p></td>
+                    <td>Due ${task.due_date}</td>
+                    <td>${task.priority}</td>
+                </tr>
+            </table>
+        `;
+
+        displayArea.appendChild(taskDiv);
+    }
+}
+
+
+//loadTaskList();
+//sortTaskList();
